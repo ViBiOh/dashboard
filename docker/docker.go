7538c9ad2454a46ff5cb66554924fec74e766f70
Vincent Boutour <bob@vibioh.fr>
2016-12-18 22:33:09 +0100

    Adding auth header

--- docker/docker.go
+++ docker/docker.go
@@ -60,16 +60,11 @@ type Handler struct {
 
 func (handler Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add(`Access-Control-Allow-Origin`, `*`)
-	w.Header().Add(`Access-Control-Allow-Headers`, `Content-Type`)
+	w.Header().Add(`Access-Control-Allow-Headers`, `Content-Type, Authorization`)
 	w.Header().Add(`Access-Control-Allow-Methods`, `GET, POST`)
 	w.Header().Add(`X-Content-Type-Options`, `nosniff`)
 
 	urlPath := []byte(r.URL.Path)
-
-	if r.Method == http.MethodOptions {
-		w.Write(nil)
-		return
-	}
 
 	if containersRequest.Match(urlPath) && r.Method == http.MethodGet {
 		containersHandler(w)
