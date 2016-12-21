c631883a79b401e9f3f5c9d775ae38066937e8db
Vincent <ViBiOh@users.noreply.github.com>
2016-12-21 15:05:56 +0100

    Update Main.jsx

--- app/Main.jsx
+++ app/Main.jsx
@@ -1,25 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router';
-import Toggle from './Toggle/Toggle';
 import style from './Main.css';
-
-const toggleProps = {
-  idle: <span>&#x2715;</span>,
-  active: <span>&#x2261;</span>,
-};
 
 const Main = ({ children }) => (
   <span className={style.mainLayout}>
-    <Toggle {...toggleProps}>
-      <nav>
-        <ul>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/">Containers</Link></li>
-          <li><hr /></li>
-          <li>&copy; ViBiOh 2016</li>
-        </ul>
-      </nav>
-    </Toggle>
     <article>{children}</article>
   </span>
 );
