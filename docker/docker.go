b52143e8bfa78cbc03ebac043f95044617aaba4d
Vincent <ViBiOh@users.noreply.github.com>
2016-12-22 10:41:00 +0100

    Update docker.go

--- docker/docker.go
+++ docker/docker.go
@@ -110,7 +110,7 @@ func restartContainer(w http.ResponseWriter, containerID []byte) {
 }
 
 func logContainer(w http.ResponseWriter, containerID []byte) {
-	logs, err := docker.ContainerLogs(context.Background(), string(containerID), types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
+	logs, err := docker.ContainerLogs(context.Background(), string(containerID), types.ContainerLogsOptions{})
 	if err != nil {
 		handleError(w, err)
 		return
