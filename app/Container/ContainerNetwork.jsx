24f7d92cb212f1b4dc4eb90bd3fbafcf63efde87
Vincent Boutour <bob@vibioh.fr>
2016-12-24 16:35:39 +0100

    Conditionning display

--- app/Container/ContainerNetwork.jsx
+++ app/Container/ContainerNetwork.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
 import style from './Containers.css';
 
-const ContainerNetwork = ({ container }) => (
-  <span className={style.container}>
-    <h3>Network</h3>
-    <span className={style.labelsContainer}>
-      {
-        Object.keys(container.NetworkSettings.Networks)
-          .map(network => (
-            <span key={network} className={style.labelItem}>
-              {network} | {container.NetworkSettings.Networks[network].IPAddress}
-            </span>
-          ))
-      }
-      {
-        Object.keys(container.NetworkSettings.Ports)
-          .filter(port => container.NetworkSettings.Ports[port])
-          .map(port => (
-            <span key={port} className={style.labelItem}>
-              {port} | {container.NetworkSettings.Ports[port].map(p => p.HostPort).join(', ')}
-            </span>
-          ))
-      }
+const ContainerNetwork = ({ container }) => {
+  if (Object.keys(container.NetworkSettings.Networks).length === 0 &&
+    Object.keys(container.NetworkSettings.Ports).length === 0) {
+    return null;
+  }
+
+  return (
+    <span className={style.container}>
+      <h3>Network</h3>
+      <span className={style.labelsContainer}>
+        {
+          Object.keys(container.NetworkSettings.Networks)
+            .map(network => (
+              <span key={network} className={style.labelItem}>
+                {network} | {container.NetworkSettings.Networks[network].IPAddress}
+              </span>
+            ))
+        }
+        {
+          Object.keys(container.NetworkSettings.Ports)
+            .filter(port => container.NetworkSettings.Ports[port])
+            .map(port => (
+              <span key={port} className={style.labelItem}>
+                {port} | {container.NetworkSettings.Ports[port].map(p => p.HostPort).join(', ')}
+              </span>
+            ))
+        }
+      </span>
     </span>
-  </span>
-);
+  );
+};
 
 ContainerNetwork.displayName = 'ContainerNetwork';
 
--- app/Container/ContainerVolumes.jsx
+++ app/Container/ContainerVolumes.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import style from './Containers.css';
 
-const ContainerVolumes = ({ container }) => (
-  <span className={style.container}>
-    <h3>Volumes</h3>
-    <span className={style.labelsContainer}>
-      {
-        container.Mounts.map((mount, index) => (
-          <span key={`mount${index}`} className={style.labelItem}>
-            <em>{mount.Source}</em> : {mount.Destination} : <strong>{mount.Mode}</strong>
-          </span>
-        ))
-      }
+const ContainerVolumes = ({ container }) => {
+  if (container.Mounts.length === 0) {
+    return null;
+  }
+
+  return (
+    <span className={style.container}>
+      <h3>Volumes</h3>
+      <span className={style.labelsContainer}>
+        {
+          container.Mounts.map((mount, index) => (
+            <span key={`mount${index}`} className={style.labelItem}>
+              <em>{mount.Source}</em> : {mount.Destination} : <strong>{mount.Mode}</strong>
+            </span>
+          ))
+        }
+      </span>
     </span>
-  </span>
-);
+  );
+};
 
 ContainerVolumes.displayName = 'ContainerVolumes';
 
