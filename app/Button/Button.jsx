3e385863c266e7a6e434741483dc0c93abf696b6
Vincent <ViBiOh@users.noreply.github.com>
2017-01-03 09:47:43 +0100

    Create Button.jsx

---
+++ app/Button/Button.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import style from './Button.css';
+
+const Button = (props) => {
+  const {children, ...buttonProps} = props;
+
+  return (
+    <button {...buttonProps}>
+      {children}
+    </button>
+  )
+};
+
+Button.displayName = 'Button';
+
+Button.propTypes = {
+  children: React.PropTypes.oneOfType([
+    React.PropTypes.arrayOf(React.PropTypes.node),
+    React.PropTypes.node,
+  ]),
+};
+
+export default Button;
