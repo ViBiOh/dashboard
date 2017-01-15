197e39f8570b3984d4c41453b8bfe2c05cb5d3b3
Vincent Boutour <bob@vibioh.fr>
2017-01-15 15:40:47 +0100

    Removing extra space

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const config = {
   context: path.join(__dirname, 'app'),
   entry: ['./index.jsx', './style.scss'],
-  
+
   resolve: {
     modulesDirectories: ['node_modules', 'src'],
     extensions: ['', '.js', '.jsx'],
