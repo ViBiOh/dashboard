13219f387f9e73ac4462aa959c531fb42f698c59
Vincent <ViBiOh@users.noreply.github.com>
2017-01-25 10:21:25 +0100

    Adding LinkDisplay

--- app/Container/ContainerInfo.jsx
+++ app/Container/ContainerInfo.jsx
@@ -28,6 +28,26 @@ const ContainerInfo = ({ container }) => {
     ];
   }
 
+  let linkContent = null;
+  if (container.HostConfig.Links.length > 0) {
+    linkContent = [
+      <h3 key="linksHeader">Links</h3>,
+      <span key="links" className={style.labels}>
+        {
+          container.HostConfig.Links.map(link => {
+            const parts = link.split(':');
+            
+            return (
+              <span key={parts[0]} className={style.item}>
+                {parts[0]} | {parts[1]}
+              </span>
+            );
+          })
+        }
+      </span>,
+    ];
+  }
+
   return (
     <span className={style.container}>
       <h2>
@@ -83,7 +103,8 @@ const ContainerInfo = ({ container }) => {
           )
         }
       </span>
-      {labelContent}
+      {labelContent}>
+      {linkContent}
     </span>
   );
 };
