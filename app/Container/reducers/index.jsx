94e6ce4a8c3db8370c397d4b8f29d4eb0d8ac185
Vincent Boutour <bob@vibioh.fr>
2017-02-07 16:49:00 +0100

    Starting to add Redux

---
+++ app/Container/reducers/index.jsx
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+const appDockerDeploy = combineReducers({});
+
+export default appDockerDeploy;
--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import Main from './Presentational/Main/Main';
+import appDockerDeploy from './Container/reducers';
 import LoginContainer from './Container/LoginContainer';
 import ContainersListContainer from './Container/ContainersListContainer';
 import ContainerContainer from './Container/ContainerContainer';
 import ComposeContainer from './Container/ComposeContainer';
 
+import Main from './Presentational/Main/Main';
+
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={ContainersListContainer} />
-      <Route path="/login" component={LoginContainer} />
-      <Route path="/containers/New" component={ComposeContainer} />
-      <Route path="/containers/:containerId" component={ContainerContainer} />
-    </Route>
-  </Router>,
+  <Provider store={createStore(appDockerDeploy)}>
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={ContainersListContainer} />
+        <Route path="/login" component={LoginContainer} />
+        <Route path="/containers/New" component={ComposeContainer} />
+        <Route path="/containers/:containerId" component={ContainerContainer} />
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
