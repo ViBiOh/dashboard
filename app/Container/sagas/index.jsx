482a280ab14857c113d9f48981e62e1a84be609b
Vincent <ViBiOh@users.noreply.github.com>
2017-02-10 09:17:34 +0100

    Removing set-error (#7)
    * Update index.jsx
    
    * Update index.jsx
    
    * Update index.jsx
    
    * Update index.jsx
    
    * Update index.jsx

--- app/Container/sagas/index.jsx
+++ app/Container/sagas/index.jsx
@@ -1,17 +1,29 @@
 import 'babel-polyfill';
-import { call, fork, put, take, takeLatest, cancel } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import { push } from 'react-router-redux';
-import { FETCH_CONTAINERS, fetchContainersSucceeded, FETCH_CONTAINER, fetchContainerSucceeded,
-  ACTION_CONTAINER_SUCCEEDED, LOGIN, LOGOUT, LOGIN_FAILED, loginSucceeded, loginFailed,
-  setError } from '../actions';
 import DockerService from '../../Service/DockerService';
+import {
+  FETCH_CONTAINERS,
+  fetchContainersSucceeded,
+  fetchContainersFailed,
+  FETCH_CONTAINER,
+  fetchContainerSucceeded,
+  fetchContainerFailed,
+  ACTION_CONTAINER_SUCCEEDED,
+  LOGIN,
+  loginSucceeded,
+  loginFailed,
+  LOGOUT,
+  logoutSucceeded,
+  logoutFailed,
+} from '../actions';
 
 function* fetchContainers() {
   try {
     const containers = yield call(DockerService.containers);
     yield put(fetchContainersSucceeded(containers));
   } catch (e) {
-    yield put(setError(e.message));
+    yield put(fetchContainersFailed(e.content));
   }
 }
 
@@ -20,31 +32,31 @@ function* fetchContainer(action) {
     const container = yield call(DockerService.infos, action.id);
     yield put(fetchContainerSucceeded(container));
   } catch (e) {
-    yield put(setError(e.message));
+    yield put(fetchContainerFailed(e.content));
   }
 }
 
 function* login(username, password) {
   try {
     yield call(DockerService.login, username, password);
-    yield put(loginSucceeded());
-    yield put(push('/'));
+    yield [
+      put(loginSucceeded()),
+      put(push('/')),
+    ];
   } catch (e) {
     yield put(loginFailed(e.content));
   }
 }
 
-function* loginFlow() {
-  while (true) { // eslint-disable-line no-constant-condition
-    const { username, password } = yield take(LOGIN);
-    const task = yield fork(login, username, password);
-
-    const action = yield take([LOGOUT, LOGIN_FAILED]);
-    if (action.type === LOGOUT) {
-      yield cancel(task);
-    }
+function* logout() {
+  try {
     yield call(DockerService.logout);
-    yield put(push('/login'));
+    yield [
+      put(logoutSucceeded()),
+      put(push('/login')),
+    ];
+  } catch (e) {
+    yield put(logoutFailed(e.content));
   }
 }
 
@@ -52,7 +64,8 @@ function* appSaga() {
   yield takeLatest(FETCH_CONTAINERS, fetchContainers);
   yield takeLatest(FETCH_CONTAINER, fetchContainer);
   yield takeLatest(ACTION_CONTAINER_SUCCEEDED, fetchContainer);
-  yield loginFlow();
+  yield takeLatest(LOGIN, login);
+  yield takeLatest(LOGOUT, logout);
 }
 
 export default appSaga;
