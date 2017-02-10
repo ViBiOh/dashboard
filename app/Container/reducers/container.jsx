5fa541a39bdf0028afb480241d2458ed2722c1e2
Vincent <ViBiOh@users.noreply.github.com>
2017-02-10 13:10:59 +0100

    Update container.jsx

--- app/Container/reducers/container.jsx
+++ app/Container/reducers/container.jsx
@@ -1,6 +1,6 @@
 import { FETCH_CONTAINER_SUCCEEDED } from '../actions';
 
-const container = (state = null, action) => {
+const container = (state = {}, action) => {
   if (action.type === FETCH_CONTAINER_SUCCEEDED) {
     return action.container;
   }
