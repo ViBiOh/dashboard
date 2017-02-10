78680bfe64d115bd4f0e0f5ee2c8588fb76f840a
Vincent <ViBiOh@users.noreply.github.com>
2017-02-10 13:00:19 +0100

    Adding fetch-container in Pending

--- app/Container/reducers/pending.jsx
+++ app/Container/reducers/pending.jsx
@@ -1,14 +1,14 @@
-import { LOGIN } from '../actions';
+import { LOGIN, FETCH_CONTAINERS, FETCH_CONTAINER } from '../actions';
 
 const endPending = /^(.*?)_(?:SUCCEEDED|FAILED)$/;
 
 const pending = (state = {}, action) => {
-  if (action.type === LOGIN) {
-    return { ...state, [LOGIN]: true };
+  if (action.type === LOGIN || action.type === FETCH_CONTAINERS || action.type === FETCH_CONTAINER) {
+    return { ...state, [action.type]: true };
   }
 
   const result = endPending.exec(action.type);
-  if (result) {
+  if (result && state[result[1]]) {
     return { ...state, [result[1]]: false };
   }
   return state;
