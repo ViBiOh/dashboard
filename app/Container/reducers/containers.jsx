94659c5a5efd1bc05a78a5e95a51051a1aa40b18
Vincent <ViBiOh@users.noreply.github.com>
2017-02-14 14:33:43 +0100

    Update containers.jsx

--- app/Container/reducers/containers.jsx
+++ app/Container/reducers/containers.jsx
@@ -1,10 +1,8 @@
 import { FETCH_CONTAINERS_SUCCEEDED } from '../actions';
 
-const containers = (state = null, action) => {
+export default (state = null, action) => {
   if (action.type === FETCH_CONTAINERS_SUCCEEDED) {
     return action.containers;
   }
   return state;
 };
-
-export default containers;
