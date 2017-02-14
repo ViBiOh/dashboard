6568b16df6cef6f66e1ea321409babfcbbb9e1e4
Vincent <ViBiOh@users.noreply.github.com>
2017-02-14 15:55:02 +0100

    Finishing actionCreator refactoring

--- app/Container/actions/index.jsx
+++ app/Container/actions/index.jsx
@@ -27,89 +27,46 @@ export const LOGOUT_FAILED = 'LOGOUT_FAILED';
 export const logoutFailed = makeActionCreator(LOGOUT_FAILED, 'error');
 
 export const FETCH_CONTAINERS = 'FETCH_CONTAINERS';
-export const fetchContainers = () => ({
-  type: FETCH_CONTAINERS,
-});
+export const fetchContainers = makeActionCreator(FETCH_CONTAINERS);
 
 export const FETCH_CONTAINERS_SUCCEEDED = 'FETCH_CONTAINERS_SUCCEEDED';
-export const fetchContainersSucceeded = containers => ({
-  type: FETCH_CONTAINERS_SUCCEEDED,
-  containers,
-});
+export const fetchContainersSucceeded = makeActionCreator(FETCH_CONTAINERS_SUCCEEDED, 'containers');
 
 export const FETCH_CONTAINERS_FAILED = 'FETCH_CONTAINERS_FAILED';
-export const fetchContainersFailed = error => ({
-  type: FETCH_CONTAINERS_FAILED,
-  error,
-});
+export const fetchContainersFailed = makeActionCreator(FETCH_CONTAINERS_FAILED, 'error');
 
 export const FETCH_CONTAINER = 'FETCH_CONTAINER';
-export const fetchContainer = id => ({
-  type: FETCH_CONTAINER,
-  id,
-});
+export const fetchContainer = makeActionCreator(FETCH_CONTAINER, 'id');
 
 export const FETCH_CONTAINER_SUCCEEDED = 'FETCH_CONTAINER_SUCCEEDED';
-export const fetchContainerSucceeded = container => ({
-  type: FETCH_CONTAINER_SUCCEEDED,
-  container,
-});
+export const fetchContainerSucceeded = makeActionCreator(FETCH_CONTAINER_SUCCEEDED, 'container');
 
 export const FETCH_CONTAINER_FAILED = 'FETCH_CONTAINER_FAILED';
-export const fetchContainerFailed = error => ({
-  type: FETCH_CONTAINER_FAILED,
-  error,
-});
+export const fetchContainerFailed = makeActionCreator(FETCH_CONTAINER_FAILED, 'error');
 
 export const ACTION_CONTAINER = 'ACTION_CONTAINER';
-export const actionContainer = (action, id) => ({
-  type: ACTION_CONTAINER,
-  action,
-  id,
-});
+export const actionContainer = makeActionCreator(ACTION_CONTAINER, 'action', 'id');
 
 export const ACTION_CONTAINER_SUCCEEDED = 'ACTION_CONTAINER_SUCCEEDED';
-export const actionContainerSucceeded = () => ({
-  type: ACTION_CONTAINER_SUCCEEDED,
-});
+export const actionContainerSucceeded = makeActionCreator(ACTION_CONTAINER_SUCCEEDED);
 
 export const ACTION_CONTAINER_FAILED = 'ACTION_CONTAINER_FAILED';
-export const actionContainerFailed = error => ({
-  type: ACTION_CONTAINER_FAILED,
-  error,
-});
+export const actionContainerFailed = makeActionCreator(ACTION_CONTAINER_FAILED, 'error');
 
 export const COMPOSE = 'COMPOSE';
-export const compose = (name, file) => ({
-  type: COMPOSE,
-  name,
-  file,
-});
+export const compose = makeActionCreator(COMPOSE, 'name', 'file');
 
 export const COMPOSE_SUCCEEDED = 'COMPOSE_SUCCEEDED';
-export const composeSucceeded = () => ({
-  type: COMPOSE_SUCCEEDED,
-});
+export const composeSucceeded = makeActionCreator(COMPOSE_SUCCEEDED);
 
 export const COMPOSE_FAILED = 'COMPOSE_FAILED';
-export const composeFailed = error => ({
-  type: COMPOSE_FAILED,
-  error,
-});
+export const composeFailed = makeActionCreator(COMPOSE_FAILED, 'error');
 
 export const OPEN_LOGS = 'OPEN_LOGS';
-export const openLogs = id => ({
-  type: OPEN_LOGS,
-  id,
-});
+export const openLogs = makeActionCreator(OPEN_LOGS, 'id');
 
 export const CLOSE_LOGS = 'CLOSE_LOGS';
-export const closeLogs = () => ({
-  type: CLOSE_LOGS,
-});
+export const closeLogs = makeActionCreator(CLOSE_LOGS);
 
 export const ADD_LOG = 'ADD_LOG';
-export const addLog = log => ({
-  type: ADD_LOG,
-  log,
-});
+export const addLog = makeActionCreator(ADD_LOG, 'log');
