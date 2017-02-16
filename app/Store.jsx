036f14357b2e2c8dc1fd6ad90a88b0f3877e06b7
Vincent <ViBiOh@users.noreply.github.com>
2017-02-16 16:14:23 +0100

    Fixing compilation

--- app/Store.jsx
+++ app/Store.jsx
@@ -9,7 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 
 const appStore = createStore(
   appReducers,
-  applyMiddleware(routerMiddleware(browserHistory), sagaMiddleware)),
+  applyMiddleware(routerMiddleware(browserHistory), sagaMiddleware),
 );
 
 sagaMiddleware.run(appSaga);
