b17bd37787dd8d9070ef3fadbe1354a3210e5426
Vincent <ViBiOh@users.noreply.github.com>
2017-03-08 09:40:46 +0100

    Adding ContainerVolumes test

---
+++ app/Presentational/Container/ContainerVolumes.test.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-env mocha */
+import { expect } from 'chai';
+import React from 'react';
+import { shallow } from 'enzyme';
+import ContainerVolumes from '../Container/ContainerVolumes';
+
+describe('Container', () => {
+  let wrapper;
+
+  const container = {
+    Mounts: [],
+  };
+
+  beforeEach(() => {
+    wrapper = shallow(<ContainerVolumes container={container} />);
+  });
+
+  it('should always render as a span', () => {
+    expect(wrapper.type()).to.equal('span');
+  });
+
+  it('should have a h3 title', () => {
+    expect(wrapper.find('h3').text()).to.equal('Volumes');
+  });
+});
