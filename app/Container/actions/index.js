796641c2a64335a5c0f03ab2558c8007dcd9d088
Vincent Boutour <bob@vibioh.fr>
2017-03-09 19:17:52 +0100

    Finishing doc

--- app/Container/actions/index.js
+++ app/Container/actions/index.js
@@ -1,8 +1,9 @@
 /**
  * Action creator : return a function for given action
- * @param  {string}    type      Action type
+ * @param  {string}     type     Action type
  * @param  {...objects} argNames Properties' names of action
- * @return {func} Function that generate action with type and properties given the params
+ * @return {func}                Function that generate action with type and properties given the
+ *                               params
  */
 const makeActionCreator = (type, ...argNames) => (...args) => {
   const action = { type };
@@ -16,15 +17,29 @@ const makeActionCreator = (type, ...argNames) => (...args) => {
 /**
  * Transform a name into a type name : SNAKE_UPPER_CASE
  * @param  {string} name A camel case action name
- * @return {string} Snake upper case type name
+ * @return {string}      Snake upper case type name
  */
-const toTypeName = name => String(name).replace(/(?!^)([A-Z])/g, '_$1').toUpperCase();
+const toTypeName = name => String(name).replace(/([A-Z])/g, '_$1').toUpperCase();
 
+/**
+ * Action creator : return the function and the constant for the given action
+ * @param  {string} type   Action type
+ * @param  {string} action Action function name
+ * @param  {Array}  inputs Properties' names of action
+ * @return {object}        An object containing both function and constant
+ */
 const makeActionAndTypeCreator = (type, action, inputs = []) => ({
   [type]: type,
   [action]: makeActionCreator(type, ...inputs),
 });
 
+/**
+ * Action creator for an API call (request, success, fail)
+ * @param  {string} camelCaseName CamelCase name of action : the action function name
+ * @param  {Array}  inputs        Properties' names of request action
+ * @param  {Array}  outputs       Properties' names of response action
+ * @return {object}               An object container constants and functions for requesting API
+ */
 function makeApiActionCreator(camelCaseName, inputs = [], outputs = []) {
   const typeName = toTypeName(camelCaseName);
 
@@ -35,12 +50,20 @@ function makeApiActionCreator(camelCaseName, inputs = [], outputs = []) {
   };
 }
 
+/**
+ * Action creator for an WebSocket call (open, close)
+ * @param  {[type]} camelCaseName CamelCase name of action : the action function name
+ * @param  {Array}  opens         Properties' names of open action
+ * @param  {Array}  closes        Properties' names of close action
+ * @return {[type]}               An object container constants and functions for requesting WS
+ */
 function makeOpenCloseActionCreator(camelCaseName, opens = [], closes = []) {
   const typeName = toTypeName(camelCaseName);
+  const camelSuffix = camelCaseName.replace(/^(.)/, (all, char) => char.toUpperCase());
 
   return {
-    ...makeActionAndTypeCreator(`OPEN_${typeName}`, `open${camelCaseName}`, opens),
-    ...makeActionAndTypeCreator(`CLOSE_${typeName}`, `close${camelCaseName}`, closes),
+    ...makeActionAndTypeCreator(`OPEN_${typeName}`, `open${camelSuffix}`, opens),
+    ...makeActionAndTypeCreator(`CLOSE_${typeName}`, `close${camelSuffix}`, closes),
   };
 }
 
@@ -51,7 +74,7 @@ export default {
   ...makeApiActionCreator('fetchContainer', ['id'], ['container']),
   ...makeApiActionCreator('actionContainer', ['action', 'id']),
   ...makeApiActionCreator('compose', ['name', 'file']),
-  ...makeOpenCloseActionCreator('Logs', ['id']),
-  ...makeOpenCloseActionCreator('Events'),
+  ...makeOpenCloseActionCreator('logs', ['id']),
+  ...makeOpenCloseActionCreator('events'),
   ...makeActionAndTypeCreator('ADD_LOG', 'addLog', ['log']),
 };
