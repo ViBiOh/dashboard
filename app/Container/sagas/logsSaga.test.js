4879c72fff7012931f2aa38ae52e713ed939795c
Vincent Boutour <bob@vibioh.fr>
2017-03-12 12:38:58 +0100

    Finishing test for logs sagas

--- app/Container/sagas/logsSaga.test.js
+++ app/Container/sagas/logsSaga.test.js
@@ -1,7 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-env mocha */
 import { expect } from 'chai';
-import { fork } from 'redux-saga/effects';
+import { fork, take, cancel } from 'redux-saga/effects';
+import { createMockTask } from 'redux-saga/utils';
+import actions from '../actions';
 import { logsSaga, readLogsSaga } from './';
 
 describe('Logs Saga', () => {
@@ -12,6 +14,30 @@ describe('Logs Saga', () => {
       iterator.next().value,
     ).to.deep.equal(
       fork(readLogsSaga, { id: 'Test' }),
+    );
+  });
+
+  it('should wait for close signal', () => {
+    const iterator = logsSaga({ id: 'Test' });
+    iterator.next();
+
+    expect(
+      iterator.next().value,
+    ).to.deep.equal(
+      take(actions.CLOSE_LOGS),
+    );
+  });
+
+  it('should cancel forked task', () => {
+    const mockTask = createMockTask();
+    const iterator = logsSaga({ id: 'Test' });
+    iterator.next();
+    iterator.next(mockTask);
+
+    expect(
+      iterator.next().value,
+    ).to.deep.equal(
+      cancel(mockTask),
     );
   });
 });
