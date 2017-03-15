cfc3d29d63a706c1ff62d481456caf46d36fb2d8
Vincent <ViBiOh@users.noreply.github.com>
2017-03-15 11:27:37 +0100

    Update Throbber.jsx

--- app/Presentational/Throbber/Throbber.jsx
+++ app/Presentational/Throbber/Throbber.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import style from './Throbber.css';
 
+/**
+ * Throbber for displaying background task.
+ * @param {Object} props Props of the component.
+ * @return {React.Component} Throbber with label and title if provided
+ */
 const Throbber = ({ label, title, className }) => (
   <div className={style.container} title={title}>
     {label && <span>{label}</span>}
