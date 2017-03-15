c3afb8a920cbd0f8edb7fa36a8de376c874ed85a
Vincent <ViBiOh@users.noreply.github.com>
2017-03-15 11:38:59 +0100

    Update ThrobberButton.jsx

--- app/Presentational/Throbber/ThrobberButton.jsx
+++ app/Presentational/Throbber/ThrobberButton.jsx
@@ -3,6 +3,11 @@ import Button from '../../Presentational/Button/Button';
 import Throbber from './Throbber';
 import style from './ThrobberButton.css';
 
+/**
+ * Throbber wrapped in a Button for displaying background task on click.
+ * @param {Object} props Props of the component.
+ * @return {React.Component} Button with wrapped children or Thorbber if pending.
+ */
 const ThrobberButton = ({ pending, onClick, children, ...buttonProps }) => (
   <Button {...buttonProps} onClick={e => (pending ? null : onClick(e))}>
     {pending ? <Throbber className={style.white} /> : children}
