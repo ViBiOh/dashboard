924737126f56d7e57bde5d7158f8c9c172a214c5
Vincent Boutour <bob@vibioh.fr>
2017-03-16 22:57:03 +0100

    Adding e2e test for starting

---
+++ test/selenium.e2e.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-env mocha */
+import { expect } from 'chai';
+import webdriver, { By } from 'selenium-webdriver';
+import test from 'selenium-webdriver/testing';
+
+test.describe('Google Search', function sequence() {
+  let driver;
+
+  this.timeout(30000);
+
+  beforeEach(() => {
+    driver = new webdriver.Builder()
+      .withCapabilities(webdriver.Capabilities.chrome())
+      .usingServer('http://ondemand.saucelabs.com:80/wd/hub')
+      .build();
+  });
+
+  afterEach(() => {
+    driver.quit();
+  });
+
+  test.it('should work', (done) => {
+    driver.get('https://www.google.com');
+
+    const searchBox = driver.findElement(By.name('q'));
+    searchBox.sendKeys('simple programmer');
+    searchBox.getAttribute('value').then((value) => {
+      expect(value).to.equal('simple programmer');
+      done();
+    });
+  });
+});
