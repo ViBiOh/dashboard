ea4d1df7c19e4455f5d11e06604cb3fb45d8f93a
Vincent <ViBiOh@users.noreply.github.com>
2017-05-04 13:26:10 +0200

    Improving documentation coverage

--- app/Tools/ref.js
+++ app/Tools/ref.js
@@ -1,3 +1,9 @@
+/**
+ * Set ref of component in ReactComponent
+ * @param {Object} that      ReactComponent context
+ * @param {String} name      Ref name
+ * @param {Object} component Component to bind to name
+ */
 export default function setRef(that, name, component) {
   that[name] = component; // eslint-disable-line no-param-reassign
 }
