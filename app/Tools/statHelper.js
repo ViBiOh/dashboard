442df3b9174142e21d8d0174dbf447576936a0ee
Vincent <ViBiOh@users.noreply.github.com>
2017-05-10 13:05:42 +0200

    Update statHelper.js

--- app/Tools/statHelper.js
+++ app/Tools/statHelper.js
@@ -10,15 +10,22 @@ export const humanSizeScale = size => Math.floor(Math.log(size) / Math.log(BYTES
 
 /**
  * Convert bytes size to human readable size.
- * @param  {int} size Bytes size
- * @param  {int} precision Decimal count
- * @param  {int} scale Human size scale
+ * @param  {Number} size Bytes size
+ * @param  {Number} precision Decimal count
+ * @param  {Number} scale Human size scale
  * @return {string}   Human readable bytes size
  */
 export const humanSize = (size, precision = 0, scale) => {
   // eslint-disable-next-line no-restricted-properties
   return `${(size / Math.pow(BYTES_SIZE, scale || humanSizeScale(size))).toFixed(precision)} ${BYTES_NAMES[i]}`;
 };
+
+/**
+ * Compute CPU maximum percentage for given stat (100% * number of CPU).
+ * @param  {Object} stat Container's stat
+ * @return {Number} Maximum CPU percentage
+ */
+export const cpuPercentageMax = stat => stat.cpu_stats.cpu_usage.percpu_usage.length * 100
 
 /**
  * Compute CPU percentage of containers.
@@ -29,7 +36,6 @@ export const computeCpuPercentage = (stat) => {
   const cpuDelta = stat.cpu_stats.cpu_usage.total_usage - stat.precpu_stats.cpu_usage.total_usage;
   const systemDelta = stat.cpu_stats.system_cpu_usage - stat.precpu_stats.system_cpu_usage;
   const ratio = cpuDelta / systemDelta;
-  const cpuCoeff = stat.cpu_stats.cpu_usage.percpu_usage.length * 100;
 
-  return parseInt(ratio * cpuCoeff * 100, 10) / 100;
+  return parseInt(ratio * cpuPercentageMax(stat) * 100, 10) / 100;
 };
