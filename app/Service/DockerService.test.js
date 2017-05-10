cb05bff2df4e544faa8f1d038cea2bc87c8b2e59
Vincent Boutour <bob@vibioh.fr>
2017-05-10 23:15:44 +0200

    Improving coverage

--- app/Service/DockerService.test.js
+++ app/Service/DockerService.test.js
@@ -67,7 +67,7 @@ test.serial('should add toString to a rejected response if JSON', t =>
   customError({
     status: 400,
     headers: new Map().set(CONTENT_TYPE_HEADER, MEDIA_TYPE_JSON),
-    json: () => Promise.resolve('{"content":"error"}'),
+    json: () => Promise.resolve({ content: 'error' }),
   })
     .then(t.fail)
     .catch((err) => {
