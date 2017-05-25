f47007b7bf9f13d1a514f03553e8ae52724792af
Vincent Boutour <bob@vibioh.fr>
2017-05-25 19:51:11 +0200

    Fixing vertical throbber

--- app/Presentational/Throbber/Throbber.jsx
+++ app/Presentational/Throbber/Throbber.jsx
@@ -7,10 +7,12 @@ import style from './Throbber.less';
  * @param {Object} props Props of the component.
  * @return {React.Component} Throbber with label and title if provided
  */
-const Throbber = ({ label, title, className }) => (
+const Throbber = ({ label, title, className, vertical, horizontalSm }) => (
   <div className={style.container} title={title}>
-    {label && <span>{label}</span>}
-    <div className={`${style.throbber} ${className}`}>
+    {!vertical && label ? <span>{label}</span> : null}
+    <div
+      className={`${style.throbber} ${vertical && style.column} ${horizontalSm && style['row-responsive']} ${className}`}
+    >
       <div className={style.bounce1} />
       <div className={style.bounce2} />
       <div className={style.bounce3} />
@@ -24,12 +26,16 @@ Throbber.propTypes = {
   label: PropTypes.string,
   title: PropTypes.string,
   className: PropTypes.string,
+  vertical: PropTypes.bool,
+  horizontalSm: PropTypes.bool,
 };
 
 Throbber.defaultProps = {
   label: '',
   title: '',
   className: '',
+  vertical: false,
+  horizontalSm: false,
 };
 
 export default Throbber;
--- app/Presentational/Throbber/ThrobberButton.jsx
+++ app/Presentational/Throbber/ThrobberButton.jsx
@@ -9,9 +9,9 @@ import style from './ThrobberButton.less';
  * @param {Object} props Props of the component.
  * @return {React.Component} Button with wrapped children or Thorbber if pending.
  */
-const ThrobberButton = ({ pending, onClick, children, ...buttonProps }) => (
+const ThrobberButton = ({ pending, vertical, horizontalSm, onClick, children, ...buttonProps }) => (
   <Button {...buttonProps} onClick={e => (pending ? null : onClick(e))}>
-    {pending ? <Throbber className={style.white} /> : children}
+    {pending ? <Throbber className={style.white} vertical horizontalSm /> : children}
   </Button>
 );
 
@@ -19,12 +19,16 @@ ThrobberButton.displayName = 'ThrobberButton';
 
 ThrobberButton.propTypes = {
   pending: PropTypes.bool,
+  vertical: PropTypes.bool,
+  horizontalSm: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
 };
 
 ThrobberButton.defaultProps = {
   pending: false,
+  vertical: false,
+  horizontalSm: false,
   children: '',
 };
 
