760a36ae56584fde3403cc523e34923145aaf30e
Vincent Boutour <vboutour@meilleursagents.com>
2017-06-14 13:11:34 +0200

    Removing button for opening logs

--- app/Presentational/Container/ContainerLogs.test.js
+++ app/Presentational/Container/ContainerLogs.test.js
@@ -1,20 +1,16 @@
 import test from 'ava';
-import sinon from 'sinon';
 import React from 'react';
 import { shallow } from 'enzyme';
 import ContainerLogs from './ContainerLogs';
 
 test('should always render as a span', (t) => {
-  const openLogs = sinon.spy();
-  const wrapper = shallow(<ContainerLogs openLogs={openLogs} />);
+  const wrapper = shallow(<ContainerLogs />);
 
   t.is(wrapper.type(), 'span');
 });
 
 test('should display logs if given', (t) => {
-  const openLogs = sinon.spy();
-  const wrapper = shallow(<ContainerLogs openLogs={openLogs} />);
-  wrapper.setProps({ logs: [] });
+  const wrapper = shallow(<ContainerLogs />);
 
   t.is(wrapper.type(), 'span');
   t.is(wrapper.find('h3').text(), 'Logs');
