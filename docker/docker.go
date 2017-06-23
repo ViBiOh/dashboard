7714bd97b86279556c4f8f4d85fe260302498cf8
Vincent <ViBiOh@users.noreply.github.com>
2017-06-23 08:35:23 +0200

    Update docker.go

--- docker/docker.go
+++ docker/docker.go
@@ -33,11 +33,11 @@ func Init(websocketOrigin string) {
 func labelFilters(filtersArgs *filters.Args, user *auth.User, appName *string) error {
 	if appName != nil && *appName != `` && isMultiApp(user) {
 		if _, err := filters.ParseFlag(`label=`+appLabel+`=`+*appName, *filtersArgs); err != nil {
-			return fmt.Errorf(`Error while parsing label for user: %v`, err)
+			return fmt.Errorf(`[%s] Error while parsing label for label=%s=%s: %v`, user.Username, appLabel, *appName, err)
 		}
 	} else if !isAdmin(user) {
 		if _, err := filters.ParseFlag(`label=`+ownerLabel+`=`+user.Username, *filtersArgs); err != nil {
-			return fmt.Errorf(`Error while parsing label for user: %v`, err)
+			return fmt.Errorf(`[%s] Error while parsing label for label=%s=%s: %v`, user.Username, ownerLabel, *user.Username, err)
 		}
 	}
 
@@ -46,31 +46,31 @@ func labelFilters(filtersArgs *filters.Args, user *auth.User, appName *string) e
 
 func eventFilters(filtersArgs *filters.Args) error {
 	if _, err := filters.ParseFlag(`event=create`, *filtersArgs); err != nil {
-		return fmt.Errorf(`Error while parsing label for user: %v`, err)
+		return fmt.Errorf(`[] Error while parsing label for event=create: %v`, err)
 	}
 	if _, err := filters.ParseFlag(`event=start`, *filtersArgs); err != nil {
-		return fmt.Errorf(`Error while parsing label for user: %v`, err)
+		return fmt.Errorf(`[] Error while parsing label for event=start: %v`, err)
 	}
 	if _, err := filters.ParseFlag(`event=stop`, *filtersArgs); err != nil {
-		return fmt.Errorf(`Error while parsing label for user: %v`, err)
+		return fmt.Errorf(`[] Error while parsing label for event=stop: %v`, err)
 	}
 	if _, err := filters.ParseFlag(`event=restart`, *filtersArgs); err != nil {
-		return fmt.Errorf(`Error while parsing label for user: %v`, err)
+		return fmt.Errorf(`[] Error while parsing label for event=restart: %v`, err)
 	}
 	if _, err := filters.ParseFlag(`event=rename`, *filtersArgs); err != nil {
-		return fmt.Errorf(`Error while parsing label for user: %v`, err)
+		return fmt.Errorf(`[] Error while parsing label for event=rename: %v`, err)
 	}
 	if _, err := filters.ParseFlag(`event=update`, *filtersArgs); err != nil {
-		return fmt.Errorf(`Error while parsing label for user: %v`, err)
+		return fmt.Errorf(`[] Error while parsing label for event=update: %v`, err)
 	}
 	if _, err := filters.ParseFlag(`event=destroy`, *filtersArgs); err != nil {
-		return fmt.Errorf(`Error while parsing label for user: %v`, err)
+		return fmt.Errorf(`[] Error while parsing label for event=destroy: %v`, err)
 	}
 	if _, err := filters.ParseFlag(`event=die`, *filtersArgs); err != nil {
-		return fmt.Errorf(`Error while parsing label for user: %v`, err)
+		return fmt.Errorf(`[] Error while parsing label for event=die: %v`, err)
 	}
 	if _, err := filters.ParseFlag(`event=kill`, *filtersArgs); err != nil {
-		return fmt.Errorf(`Error while parsing label for user: %v`, err)
+		return fmt.Errorf(`[] Error while parsing label for event=kill: %v`, err)
 	}
 
 	return nil
