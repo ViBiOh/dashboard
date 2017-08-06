5bb816d774b4e3109f2838340492b772f058b3a4
Vincent Boutour <bob@vibioh.fr>
2017-08-06 20:06:22 +0200

    Adding get username endpoint

--- oauth/github/github.go
+++ oauth/github/github.go
@@ -3,6 +3,7 @@ package github
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
@@ -33,21 +34,21 @@ func Init() {
 	}
 }
 
-func getAccessToken(requestState string, requestCode string) (*oauth2.Token, error) {
+func getAccessToken(requestState string, requestCode string) (string, error) {
 	if state != requestState {
-		return nil, fmt.Errorf(`Invalid state provided for oauth`)
+		return ``, fmt.Errorf(`Invalid state provided for oauth`)
 	}
 
 	token, err := oauthConf.Exchange(oauth2.NoContext, requestCode)
 	if err != nil {
-		return nil, fmt.Errorf(`Invalid code provided for oauth`)
+		return ``, fmt.Errorf(`Invalid code provided for oauth`)
 	}
 
-	return token, nil
+	return token.AccessToken, nil
 }
 
-func getUsername(token *oauth2.Token) (string, error) {
-	userResponse, err := fetch.GetBody(userURL, token.AccessToken)
+func getUsername(token string) (string, error) {
+	userResponse, err := fetch.GetBody(userURL, token)
 	if err != nil {
 		return ``, fmt.Errorf(`Error while fetching user informations: %v`, err)
 	}
@@ -69,8 +70,15 @@ func (handler Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		if token, err := getAccessToken(r.FormValue(`state`), r.FormValue(`code`)); err != nil {
 			http.Error(w, err.Error(), http.StatusUnauthorized)
 		} else {
-			log.Printf(`Token: %s`, token.AccessToken)
-			w.Write([]byte(token.AccessToken))
+			log.Printf(`Token: %s`, token)
+			w.Write([]byte(token))
+		}
+	} else if r.URL.Path == `/user` {
+		if username, err := getUsername(r.Header.Get(`Authorization`)); err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+		} else {
+			log.Printf(`Username: %s`, username)
+			w.Write([]byte(username))
 		}
 	}
 }
