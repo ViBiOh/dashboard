50714df655e4bf2a74caa499f939938de17a4b23
Vincent Boutour <bob@vibioh.fr>
2017-08-11 00:25:29 +0200

    Adding test for service full name

--- docker/compose_test.go
+++ docker/compose_test.go
@@ -4,6 +4,26 @@ import (
 	"testing"
 )
 
+func TestGetServiceFullName(t *testing.T) {
+	var tests = []struct {
+		app     string
+		service string
+		want    string
+	}{
+		{
+			`dashboard`,
+			`api`,
+			`dashboard_api_deploy`,
+		},
+	}
+
+	for _, test := range tests {
+		if result := getServiceFullName(test.app, test.service); result != test.want {
+			t.Errorf(`getServiceFullName(%v, %v) = %v, want %v`, test.app, test.service, result, test.want)
+		}
+	}
+}
+
 func TestGetFinalName(t *testing.T) {
 	var tests = []struct {
 		serviceFullName string
