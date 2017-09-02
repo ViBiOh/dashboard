c2fae81ed709dcc2c7d2a0f537eef435627719fd
Vincent Boutour <bob@vibioh.fr>
2017-09-02 13:51:25 +0200

    Removing obsolete env in compose, restoring e2e test with logs

--- e2e.sh
+++ e2e.sh
@@ -5,7 +5,10 @@ set -e
 echo Starting Dashboard with local configuration
 export GIT_COMMIT=`git log --pretty=format:'%h' -n 1`
 docker-compose -p dashboard -f docker-compose-local.yml up -d
+
+echo Checking everything started fine
 docker ps -a
+docker-compose -p dashboard -f docker-compose-local.yml logs
 
 echo Running e2e tests
 npm run test:e2e
