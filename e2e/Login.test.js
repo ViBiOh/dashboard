130b4f6cfab9d6f743e29f900448dd4a21f60f38
Vincent Boutour <bob@vibioh.fr>
2017-09-02 18:10:49 +0200

    Addig wait onlogin

--- e2e/Login.test.js
+++ e2e/Login.test.js
@@ -3,5 +3,6 @@ Feature('Login');
 Scenario('Basic auth login', (I, loginPage) => {
   I.amOnPage('/login');
   loginPage.basicLogin('admin', 'admin');
+  I.waitUrlEquals('/', 5);
   I.see('vibioh/dashboard');
 });
