5115b99e025acb8ab5af86f8fe3d6e00e45ce887
Vincent <ViBiOh@users.noreply.github.com>
2017-09-05 10:55:59 +0200

    Update websocket.go

--- docker/websocket.go
+++ docker/websocket.go
@@ -257,7 +257,6 @@ type WebsocketHandler struct {
 }
 
 func (handler WebsocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf(`request: %v`, r)
 	if strings.HasPrefix(r.URL.Path, busPrefix) {
 		busWebsocketHandler(w, r)
 	}
