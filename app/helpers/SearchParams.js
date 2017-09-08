395308103d0280475e83f6388f17e074f2f1dae6
Vincent Boutour <vboutour@meilleursagents.com>
2017-09-08 09:44:32 +0200

    Simplifying regex syntax

--- app/helpers/SearchParams.js
+++ app/helpers/SearchParams.js
@@ -1,6 +1,6 @@
 export default function (content) {
   const params = {};
-  content.replace(/([^?&=]+)(?:=([^?&=]*))?/g, (match, key, value) => {
+  content.replace(/\??([^=]+)=?([^&]*)&?/g, (match, key, value) => {
     // eslint-disable-next-line no-param-reassign
     params[key] = typeof value === 'undefined' ? true : decodeURIComponent(value);
   });
