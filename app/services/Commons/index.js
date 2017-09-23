00f64cb5cd57f4572d8596591ae2c3daf04081f7
Vincent Boutour <bob@vibioh.fr>
2017-09-23 17:52:34 +0200

    Adding default http error message

--- app/services/Commons/index.js
+++ app/services/Commons/index.js
@@ -3,6 +3,42 @@ import { STORAGE_KEY_AUTH } from '../../Constants';
 import localStorage from '../LocalStorage';
 
 /**
+ * Default HTTP error message.
+ * @type {Object}
+ */
+const httpErrorMessage = {
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  402: 'Payment Required',
+  403: 'Forbidden',
+  404: 'Not Found',
+  405: 'Method Not Allowed',
+  406: 'Not Acceptable',
+  407: 'Proxy Authentication Required',
+  408: 'Request Time-out',
+  409: 'Conflict',
+  410: 'Gone',
+  411: 'Length Required',
+  412: 'Precondition Failed',
+  413: 'Request Entity Too Large',
+  414: 'Request-URI Too Long',
+  415: 'Unsupported Media Type',
+  416: 'Requested range unsatisfiable',
+  417: 'Expectation failed',
+  418: 'I’m a teapot',
+  421: 'Bad mapping / Misdirected Request',
+  426: 'Upgrade Required',
+  428: 'Precondition Required',
+  429: 'Too Many Requests',
+  431: 'Request Header Fields Too Large',
+  500: 'Internal Server Error',
+  501: 'Not Implemented',
+  502: 'Bad Gateway ou Proxy Error',
+  503: 'Service Unavailable',
+  504: 'Gateway Time-out',
+};
+
+/**
  * Custom error handler that add toString to error object.
  * @param  {Object} response Response from funtch
  * @return {Promise} Error with toString or valid reponse
@@ -18,6 +54,8 @@ export function customError(response) {
           toString: () => {
             if (typeof err.content === 'string') {
               return err.content;
+            } else if (!err.content && httpErrorMessage[err.status]) {
+              return httpErrorMessage[err.status];
             }
             return JSON.stringify(err.content);
           },
--- app/services/Commons/index.test.js
+++ app/services/Commons/index.test.js
@@ -14,6 +14,18 @@ test('should add toString to a rejected response', t =>
       t.is(String(err), 'error');
     }));
 
+test('should add http error to a rejected response', t =>
+  customError({
+    status: 400,
+    headers: new Map(),
+    text: () => Promise.resolve(),
+  })
+    .then(t.fail)
+    .catch((err) => {
+      t.true(typeof err.toString === 'function');
+      t.is(String(err), 'Bad Request');
+    }));
+
 test('should add toString to a rejected response if JSON', t =>
   customError({
     status: 400,
