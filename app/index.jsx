1d0092d0c3badbddd647926015c7a550da7b844b
Vincent Boutour <bob@vibioh.fr>
2017-10-01 13:59:46 +0200

    Removing google analytics

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import ReactGA from 'react-ga';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import actions from './actions';
-import { init, getGaId } from './Constants';
+import { init } from './Constants';
 import appStore from './Store';
 import App from './App';
 
 init().then(() => {
   if (!/auth/.test(document.location.pathname)) {
     appStore.dispatch(actions.info());
-  }
-
-  const gaId = getGaId();
-  if (gaId) {
-    ReactGA.initialize(gaId);
-    ReactGA.pageview(window.location.pathname + window.location.search);
   }
 
   ReactDOM.render(
