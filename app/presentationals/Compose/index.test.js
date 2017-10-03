758998b05640ed77d805dc1af980cfeb1eb100a8
Vincent Boutour <bob@vibioh.fr>
2017-10-03 21:54:05 +0200

    Removing mount on test

--- app/presentationals/Compose/index.test.js
+++ app/presentationals/Compose/index.test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import sinon from 'sinon';
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Compose from './';
 
 const defaultProps = {
@@ -10,40 +10,13 @@ const defaultProps = {
 };
 
 test('should render into a span', (t) => {
-  const wrapper = mount(<Compose {...defaultProps} />);
+  const wrapper = shallow(<Compose {...defaultProps} />);
 
   t.true(wrapper.find('span').length >= 1);
 });
 
 test('should have a h2 title', (t) => {
-  const wrapper = mount(<Compose {...defaultProps} />);
+  const wrapper = shallow(<Compose {...defaultProps} />);
 
   t.is(wrapper.find('h2').text(), 'Create an app');
-});
-
-test('should call given onCompose method', (t) => {
-  const onCompose = sinon.spy();
-  const wrapper = mount(<Compose {...defaultProps} onCompose={onCompose} />);
-
-  wrapper.find('ThrobberButton').simulate('click');
-
-  t.true(onCompose.called);
-});
-
-test('should call submit on enter key down', (t) => {
-  const onCompose = sinon.spy();
-  const wrapper = mount(<Compose {...defaultProps} onCompose={onCompose} />);
-
-  wrapper.find('input').simulate('keyDown', { keyCode: 13 });
-
-  t.true(onCompose.called);
-});
-
-test('should not call submit on other key down', (t) => {
-  const onCompose = sinon.spy();
-  const wrapper = mount(<Compose {...defaultProps} onCompose={onCompose} />);
-
-  wrapper.find('textarea').simulate('keyDown', { keyCode: 10 });
-
-  t.false(onCompose.called);
 });
