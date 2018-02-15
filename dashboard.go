7db532de781256679418730394de78501e561b8d
Vincent Boutour <vboutour@meilleursagents.com>
2018-02-15 12:57:17 +0100

    Using new httputils

--- dashboard.go
+++ dashboard.go
@@ -2,28 +2,20 @@ package main
 
 import (
 	"errors"
-	"flag"
 	"log"
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/NYTimes/gziphandler"
-	"github.com/ViBiOh/alcotest/alcotest"
 	"github.com/ViBiOh/auth/auth"
 	"github.com/ViBiOh/dashboard/docker"
 	"github.com/ViBiOh/httputils"
-	"github.com/ViBiOh/httputils/cert"
 	"github.com/ViBiOh/httputils/cors"
 	"github.com/ViBiOh/httputils/owasp"
 )
 
 const websocketPrefix = `/ws`
-
-var (
-	restHandler      http.Handler
-	websocketHandler http.Handler
-)
 
 func handleGracefulClose() error {
 	if docker.CanBeGracefullyClosed() {
@@ -45,53 +37,27 @@ func handleGracefulClose() error {
 	}
 }
 
-func dashboardHandler(w http.ResponseWriter, r *http.Request) {
-	if strings.HasPrefix(r.URL.Path, websocketPrefix) {
-		websocketHandler.ServeHTTP(w, r)
-	} else {
-		restHandler.ServeHTTP(w, r)
-	}
-}
-
 func main() {
-	port := flag.String(`port`, `1080`, `Listen port`)
-	tls := flag.Bool(`tls`, true, `Serve TLS content`)
-	alcotestConfig := alcotest.Flags(``)
-	tlsConfig := cert.Flags(`tls`)
 	authConfig := auth.Flags(`auth`)
 	owaspConfig := owasp.Flags(``)
 	corsConfig := cors.Flags(`cors`)
-	flag.Parse()
 
-	alcotest.DoAndExit(alcotestConfig)
+	httputils.StartMainServer(func() http.Handler {
+		authApp := auth.NewApp(authConfig, nil)
 
-	log.Print(`Starting server on port ` + *port)
-
-	authApp := auth.NewApp(authConfig, nil)
-
-	if err := docker.Init(authApp); err != nil {
-		log.Printf(`Error while initializing docker: %v`, err)
-	}
-
-	restHandler = gziphandler.GzipHandler(owasp.Handler(owaspConfig, cors.Handler(corsConfig, docker.Handler())))
-	websocketHandler = http.StripPrefix(websocketPrefix, docker.WebsocketHandler())
-
-	server := &http.Server{
-		Addr:    `:` + *port,
-		Handler: http.HandlerFunc(dashboardHandler),
-	}
-
-	var serveError = make(chan error)
-	go func() {
-		defer close(serveError)
-		if *tls {
-			log.Print(`Listening with TLS enabled`)
-			serveError <- cert.ListenAndServeTLS(tlsConfig, server)
-		} else {
-			log.Print(`⚠ dashboard is running without secure connection ⚠`)
-			serveError <- server.ListenAndServe()
+		if err := docker.Init(authApp); err != nil {
+			log.Fatalf(`Error while initializing docker: %v`, err)
 		}
-	}()
 
-	httputils.ServerGracefulClose(server, serveError, handleGracefulClose)
+		restHandler := gziphandler.GzipHandler(owasp.Handler(owaspConfig, cors.Handler(corsConfig, docker.Handler())))
+		websocketHandler := http.StripPrefix(websocketPrefix, docker.WebsocketHandler())
+
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasPrefix(r.URL.Path, websocketPrefix) {
+				websocketHandler.ServeHTTP(w, r)
+			} else {
+				restHandler.ServeHTTP(w, r)
+			}
+		})
+	}, handleGracefulClose)
 }
