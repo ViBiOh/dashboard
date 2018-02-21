65cc8e370d0848b2593110f16b42a26062e6d32f
Vincent Boutour <bob@vibioh.fr>
2018-02-21 23:44:08 +0100

    Updating revert part

--- deploy.sh
+++ deploy.sh
@@ -32,13 +32,14 @@ function docker-compose-deploy() {
 
     docker-compose -p ${PROJECT_FULLNAME} logs || true
     docker-compose -p ${PROJECT_FULLNAME} ps -q | xargs docker inspect --format='{{ .Name }}{{ "\n" }}{{range .State.Health.Log }}code={{ .ExitCode }}, log={{ .Output }}{{ end }}' || true
-    docker-compose -p ${PROJECT_FULLNAME} rm --force --stop -v || true
+    docker-compose -p ${PROJECT_FULLNAME} stop -t 180 || true
+    docker-compose -p ${PROJECT_FULLNAME} rm -f -v || true
     return 1
   fi
 
   if [ ! -z "${oldServices}" ]; then
     echo Stopping old containers
-    docker stop --time=180 ${oldServices} || true
+    docker stop -t 180 ${oldServices} || true
   fi
 
   if [ ! -z "${oldServices}" ]; then
