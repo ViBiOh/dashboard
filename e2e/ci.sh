f9071405389a38c8b598aab20bbec5d7f84185c3
Vincent Boutour <bob@vibioh.fr>
2018-03-24 16:43:11 +0100

    Moving e2e files into their own dir

--- e2e/ci.sh
+++ e2e/ci.sh
@@ -4,21 +4,23 @@ set -e
 
 echo Starting Dashboard with local configuration
 
-./bin/compose \
+go run tools/compose.go \
   -tls=false \
   -authBasic \
   -traefik=false \
   -github=false \
   -selenium=true \
   -domain=:1080 \
-  -version=`git log --pretty=format:'%h' -n 1` > docker-compose.e2e.yml
+  -version=`git log --pretty=format:'%h' -n 1` > docker-compose.yml
 
 export ADMIN_PASSWORD=`bcrypt admin`
-docker-compose -p dashboard -f docker-compose.e2e.yml up -d
+docker-compose -p dashboard -f docker-compose.yml up -d
 
 set +e
 
 echo Running e2e tests
+
+npm install
 npm run test:e2e
 result=$?
 
@@ -26,10 +28,10 @@ set -e
 
 if [ "${result}" != "0" ]; then
     echo Checking logs on failure
-    docker-compose -p dashboard -f docker-compose.e2e.yml logs
+    docker-compose -p dashboard -f docker-compose.yml logs
 fi
 
 echo Stopping started containers
-docker-compose -p dashboard -f docker-compose.e2e.yml stop
+docker-compose -p dashboard -f docker-compose.yml stop
 
 exit $result
