d9fde10310e08c84c27d105c6c9105bda7d6ae73
Vincent Boutour <bob@vibioh.fr>
2018-10-01 22:41:43 +0200

    Changing typo in test

--- e2e/Login.test.js
+++ e2e/Login.test.js
@@ -5,7 +5,7 @@ Scenario('Basic auth error', (I, loginPage) => {
   loginPage.basicLogin('admin', 'invalid');
   I.waitForVisible(loginPage.error, 5);
   I.seeElement(loginPage.error);
-  I.see('Invalid credentials for admin');
+  I.see('invalid credentials for admin');
 });
 
 Scenario('Basic auth success', (I, loginPage, listPage) => {
