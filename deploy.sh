4e9a2e5b7a3f2205ca8da4f7aaa475ef22ac1c45
Vincent Boutour <bob@vibioh.fr>
2019-01-03 22:33:34 +0100

    Updating script syntax

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,35 @@
 #!/usr/bin/env bash
 
-set -e
-set -u
+set -o errexit
+set -o nounset
+set -o pipefail
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-function readVariableIfRequired() {
-  if [ -z "${!1}" ]; then
+readVariableIfRequired() {
+  if [[ -z "${!1}" ]]; then
     read -p "${1}=" $1
   else
     echo "${1}="${!1}
   fi
 }
 
-function docker-compose-deploy() {
+docker-compose-deploy() {
   local PROJECT_NAME="${1}"
   readVariableIfRequired "PROJECT_NAME"
 
-  local oldServices=`docker ps -f name="${PROJECT_NAME}*" -q`
-  local PROJECT_FULLNAME=${PROJECT_NAME}`git rev-parse --short HEAD`
+  local oldServices=$(docker ps -f name="${PROJECT_NAME}*" -q)
+  local PROJECT_FULLNAME="${PROJECT_NAME}$(git rev-parse --short HEAD)"
 
   docker-compose -p "${PROJECT_FULLNAME}" config -q
   docker-compose -p "${PROJECT_FULLNAME}" pull
   docker-compose -p "${PROJECT_FULLNAME}" up -d
-  local servicesCount=`docker-compose -p "${PROJECT_FULLNAME}" ps -q | wc -l`
+  local servicesCount=$(docker-compose -p "${PROJECT_FULLNAME}" ps -q | wc -l)
 
   echo "Waiting 45 seconds for containers to start..."
-  timeout=`date --date="45 seconds" +%s`
-  local healthyCount=`docker events --until "${timeout}" -f event="health_status: healthy" -f name="${PROJECT_FULLNAME}" | wc -l`
+  timeout=$(date --date="45 seconds" +%s)
+  local healthyCount=$(docker events --until "${timeout}" -f event="health_status: healthy" -f name="${PROJECT_FULLNAME}" | wc -l)
 
-  if [ "${servicesCount}" -gt "${healthyCount}" ]; then
+  if [[ "${servicesCount}" -gt "${healthyCount}" ]]; then
     echo "Containers didn't start, reverting..."
 
     docker-compose -p "${PROJECT_FULLNAME}" logs || true
@@ -36,20 +38,20 @@ function docker-compose-deploy() {
     return 1
   fi
 
-  if [ ! -z "${oldServices}" ]; then
+  if [[ ! -z "${oldServices}" ]]; then
     echo "Stopping old containers ${oldServices}"
     docker stop --time=180 ${oldServices}
   fi
 
-  if [ ! -z "${oldServices}" ]; then
+  if [[ ! -z "${oldServices}" ]]; then
     echo "Removing old containers ${oldServices}"
     docker rm -f -v ${oldServices}
   fi
 
   echo Renaming containers
 
-  for service in `docker-compose -p "${PROJECT_FULLNAME}" ps --services`; do
-      local containerID=`docker ps -q --filter name="${PROJECT_FULLNAME}_${service}"`
+  for service in $(docker-compose -p "${PROJECT_FULLNAME}" ps --services); do
+      local containerID=$(docker ps -q --filter name="${PROJECT_FULLNAME}_${service}")
       docker rename "${containerID}" "${PROJECT_NAME}_${service}"
   done
 
@@ -58,28 +60,32 @@ function docker-compose-deploy() {
   docker system prune -f || true
 }
 
-export PATH=${PATH}:/opt/bin
+main() {
+  export PATH=${PATH}:/opt/bin
 
-PROJECT_NAME=${1}
-readVariableIfRequired "PROJECT_NAME"
+  PROJECT_NAME=${1}
+  readVariableIfRequired "PROJECT_NAME"
 
-PROJECT_URL=${2}
-readVariableIfRequired "PROJECT_URL"
+  PROJECT_URL=${2}
+  readVariableIfRequired "PROJECT_URL"
 
-if [ ! -d "${PROJECT_NAME}" ]; then
-  git clone ${PROJECT_URL} ${PROJECT_NAME}
-fi
+  if [[ ! -d "${PROJECT_NAME}" ]]; then
+    git clone ${PROJECT_URL} ${PROJECT_NAME}
+  fi
 
-cd ${PROJECT_NAME}
-git pull
+  cd ${PROJECT_NAME}
+  git pull
 
-echo "Deploying ${PROJECT_NAME}"
-docker-compose-deploy ${PROJECT_NAME}
+  echo "Deploying ${PROJECT_NAME}"
+  docker-compose-deploy ${PROJECT_NAME}
 
-if [ -n "${ROLLBAR_TOKEN}" ]; then
-  curl https://api.rollbar.com/api/1/deploy/ \
-    -F access_token=${ROLLBAR_TOKEN} \
-    -F environment=prod \
-    -F revision=`make version` \
-    -F local_username=`make author`
-fi
+  if [[ -n "${ROLLBAR_TOKEN}" ]]; then
+    curl https://api.rollbar.com/api/1/deploy/ \
+      -F access_token=${ROLLBAR_TOKEN} \
+      -F environment=prod \
+      -F revision=$(make version) \
+      -F local_username=$(make author)
+  fi
+}
+
+main "${@}"
--- e2e.sh
+++ e2e.sh
@@ -1,63 +1,69 @@
 #!/usr/bin/env bash
 
-set -e
-set -u
+set -o errexit
+set -o nounset
+set -o pipefail
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export PROJECT_NAME='dashboard'
 
-function renameComposeContainer() {
+renameComposeContainer() {
   local serviceNum=1
-  for container in `docker-compose -p "${PROJECT_NAME}" -f docker-compose.yml ps -q`; do
-      local serviceName=`docker-compose -p "${PROJECT_NAME}" -f docker-compose.yml ps --services | sed "${serviceNum}q;d"`
+  for container in $(docker-compose -p "${PROJECT_NAME}" -f docker-compose.yml ps -q); do
+      local serviceName=$(docker-compose -p "${PROJECT_NAME}" -f docker-compose.yml ps --services | sed "${serviceNum}q;d")
       docker rename "${container}" "${PROJECT_NAME}_${serviceName}"
       ((serviceNum++))
   done
 }
 
-echo Starting Dashboard with local configuration
+main() {
+  echo Starting Dashboard with local configuration
 
-go run cmd/compose/compose.go \
-  -authBasic \
-  -domain=:1080 \
-  -environment="e2e" \
-  -github=false \
-  -mailer=false \
-  -selenium=true \
-  -tls=false \
-  -tracing=false \
-  -rollbar=false \
-  -traefik=false \
-  -version=`git log --pretty=format:'%h' -n 1` \
-  > docker-compose.yml
+  go run cmd/compose/compose.go \
+    -authBasic \
+    -domain=:1080 \
+    -environment="e2e" \
+    -github=false \
+    -mailer=false \
+    -selenium=true \
+    -tls=false \
+    -tracing=false \
+    -rollbar=false \
+    -traefik=false \
+    -version=$(git log --pretty=format:'%h' -n 1) \
+    > docker-compose.yml
 
-go get github.com/ViBiOh/auth/cmd/bcrypt
-export ADMIN_PASSWORD=`bcrypt admin`
+  go get github.com/ViBiOh/auth/cmd/bcrypt
+  export ADMIN_PASSWORD=$(bcrypt admin)
 
-docker-compose -p "${PROJECT_NAME}" -f docker-compose.yml up -d
-renameComposeContainer
+  docker-compose -p "${PROJECT_NAME}" -f docker-compose.yml up -d
+  renameComposeContainer
 
-set +e
+  set +e
 
-echo Running e2e tests
+  echo Running e2e tests
 
-docker run \
-  -it \
-  --rm \
-  --network "${PROJECT_NAME}_default" \
-  --link "${PROJECT_NAME}_selenium":selenium \
-  -v `pwd`/e2e:/tests codeception/codeceptjs \
-  codeceptjs run-multiple --all
-result=$?
+  docker run \
+    -it \
+    --rm \
+    --network "${PROJECT_NAME}_default" \
+    --link "${PROJECT_NAME}_selenium":selenium \
+    -v $(pwd)/e2e:/tests codeception/codeceptjs \
+    codeceptjs run-multiple --all
+  result=$?
 
-set -e
+  set -e
 
-if [ "${result}" != "0" ]; then
-    echo Checking logs on failure
-    docker-compose -p "${PROJECT_NAME}" -f docker-compose.yml logs
-fi
+  if [[ "${result}" != "0" ]]; then
+      echo Checking logs on failure
+      docker-compose -p "${PROJECT_NAME}" -f docker-compose.yml logs
+  fi
 
-echo Stopping started containers
-docker-compose -p "${PROJECT_NAME}" -f docker-compose.yml stop
-docker-compose -p "${PROJECT_NAME}" -f docker-compose.yml rm -f -v
+  echo Stopping started containers
+  docker-compose -p "${PROJECT_NAME}" -f docker-compose.yml stop
+  docker-compose -p "${PROJECT_NAME}" -f docker-compose.yml rm -f -v
 
-exit $result
+  exit $result
+}
+
+main "${@}"
