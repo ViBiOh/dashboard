30f79ffff0523b7a715aff2dd6e2509dcca0ea2a
Vincent Boutour <bob@vibioh.fr>
2019-03-25 08:23:05 +0100

    Cleaning behind me

--- deploy.sh
+++ deploy.sh
@@ -6,16 +6,18 @@ set -o pipefail
 
 
 main() {
-  export PATH=${PATH}:/opt/bin
+  export PATH="${PATH}:/opt/bin"
 
   curl -O https://raw.githubusercontent.com/ViBiOh/docker-compose-deploy/master/deploy.sh
   chmod +x deploy.sh
 
-  curl -o docker-compose-dashboard.yml https://raw.githubusercontent.com/ViBiOh/dashboard/master/docker-compose.yml
+  local COMPOSE_FILE="docker-compose-dashboard.yml"
+  curl -o "${COMPOSE_FILE}" https://raw.githubusercontent.com/ViBiOh/dashboard/master/docker-compose.yml
 
-  ./deploy.sh "dashboard" "${1:-SHA1}" docker-compose-dashboard.yml
+  ./deploy.sh "dashboard" "${1:-SHA1}" "${COMPOSE_FILE}"
 
   set +e
+  rm -rf deploy.sh "${COMPOSE_FILE}"
   docker system prune -f
   set -e
 }
