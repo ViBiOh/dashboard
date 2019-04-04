0555f518b47762e5db2a21bd4aaeae7bfadcc263
Vincent Boutour <bob@vibioh.fr>
2019-04-04 22:46:47 +0200

    Shortening bash option declaration and removing extra declaration

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
+set -o nounset -o pipefail -o errexit
 
 main() {
   curl -O https://raw.githubusercontent.com/ViBiOh/docker-compose-deploy/master/deploy.sh
